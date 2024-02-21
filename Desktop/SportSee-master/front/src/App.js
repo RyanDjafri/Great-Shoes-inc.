cf1448324a60eaaa49eccc1d54fd6e33bf67a328
RyanDjafri <ryandjafripro@gmail.com>
2024-02-21 19:03:48 +0100

    navbar, sidebar

---
+++ Desktop/SportSee-master/front/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
